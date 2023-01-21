a4711390055207ce0cbfe96a9e6d798b05827191
Zet-a <66399177+Zet-a@users.noreply.github.com>
2023-01-21 14:44:18 -0600

    Update Oak.lua

--- Oak.lua
+++ Oak.lua
@@ -1,117 +1,111 @@
-local zlib = loadstring(game:HttpGet('https://raw.githubusercontent.com/Zet-a/RobIox/main/LibrarySnippet.lua'))()
+-- Fixed by squares (thanks) | L devs
+local zlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zet-a/RobIox/main/LibrarySnippet.lua"))()
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/Library.lua"))()
 local rocks = workspace.World.RockRegions.Island
-local orewhitelist = {"Gold","Mythril"}
 local selected = "Mythril"
-local instance = {
-    Mythril = {
-        color = Color3.fromRGB(90,166,165)
-    },
-    Gold = {
-        color = Color3.fromRGB(221,146,17)
-    },
-    Iron = {
-        color = Color3.fromRGB(111,107,118)
-    },
-    Pyrite = {
-        color = Color3.fromRGB(207,165,15)
-    },
-    Tin = {
-        color = Color3.fromRGB(229,228,223)
-    },
-    Copper = {
-        color = Color3.fromRGB(206,98,35)
-    }
+local ores = {
+	Mythril = {
+		["rbxassetid://12100886066"] = BrickColor.new("Med. bluish green"),
+		["rbxassetid://12100886051"] = BrickColor.new("Med. bluish green"),
+		["rbxassetid://12100886068"] = BrickColor.new("Med. bluish green")
+	},
+	Gold = {
+		["rbxassetid://12100885942"] = BrickColor.new("Br. yellowish orange"),
+		["rbxassetid://12100885949"] = BrickColor.new("Br. yellowish orange"),
+		["rbxassetid://12100885963"] = BrickColor.new("Br. yellowish orange")
+	},
+	Iron = {
+		["rbxassetid://12100886003"] = BrickColor.new("Dark grey"),
+		["rbxassetid://12100886012"] = BrickColor.new("Dark grey"),
+		["rbxassetid://12100886011"] = BrickColor.new("Dark grey")
+	},
+	Pyrite = {
+		["rbxassetid://12100885937"] = BrickColor.new("Flame yellowish orange"),
+		["rbxassetid://12100885933"] = BrickColor.new("Flame yellowish orange"),
+		["rbxassetid://12100885936"] = BrickColor.new("Flame yellowish orange")
+	},
+	Tin = {
+		["rbxassetid://12100886049"] = BrickColor.new("Light stone grey"),
+		["rbxassetid://12100886035"] = BrickColor.new("Light stone grey"),
+		["rbxassetid://12100886020"] = BrickColor.new("Light stone grey")
+	},
+	Copper = {
+		["rbxassetid://12100885998"] = BrickColor.new("Flame reddish orange"),
+		["rbxassetid://12100885960"] = BrickColor.new("Flame reddish orange"),
+		["rbxassetid://12100885958"] = BrickColor.new("Flame reddish orange")
+	},
 }
-local currentinstances = {
 
-}
+local function addOre(ore)
+	if ore:IsA("MeshPart") then
+		for i, v in pairs(ores) do
+			for o, b in pairs(v) do
+				if tostring(ore.MeshId) == o then
+					zlib:box(ore.Parent.Parent, i, {
+						Color = b.Color,
+					})
+					zlib:text(ore.Parent.Parent, -2, 0, i .. "title", {
+						Text = i,
+						Visible = false,
+						Outline = true,
+						OutlineColor = Color3.fromRGB(0, 0, 0),
+						Color = b.Color,
+						Center = true,
+						Font = 2,
+					})
+				end
+			end
+		end
+	end
+end
 
-for i,v in pairs(rocks:GetDescendants()) do
-    for a,b in pairs(instance) do
-        if string.find(v.Name,a) then
-            local instance = v.Parent.Parent
-            if not table.find(currentinstances,instance) then
-                table.insert(currentinstances,instance)
-                zlib:box(instance,string.match(v.Name,a),{
-                     Color = b.color
-                })
-                zlib:text(instance,-2,0,string.match(v.Name,a).."title",{
-                    Text = string.match(v.Name,a),
-                    Visible = false,
-                    Outline = true,
-                    OutlineColor = Color3.fromRGB(0,0,0),
-                    Color = b.color,
-                    Center = true,
-                    Font = 2
-                })
-            end
-        end
-    end
+for _, v in pairs(rocks:GetDescendants()) do
+	addOre(v)
 end
 rocks.DescendantAdded:Connect(function(v)
-    for a,b in pairs(instance) do
-        if string.find(v.Name,a) then
-            local instance = v.Parent.Parent
-            if not table.find(currentinstances,instance) then
-                table.insert(currentinstances,instance)
-                zlib:box(instance,string.match(v.Name,a),{
-                     Color = b.color
-                })
-                zlib:text(instance,-2,0,string.match(v.Name,a).."title",{
-                    Text = string.match(v.Name,a),
-                    Visible = false,
-                    Outline = true,
-                    OutlineColor = Color3.fromRGB(0,0,0),
-                    Color = b.color,
-                    Center = true,
-                    Font = 2
-                })
-            end
-        end
-    end
+	addOre(v)
 end)
 
 local window = lib:CreateWindow({
-    Title = 'Obsidian (Oaklands)',
-    Center = true,
-    AutoShow = true,
+	Title = "Obsidian (Oaklands)",
+	Center = true,
+	AutoShow = true,
 })
 
 local Tabs = {
-    Main = window:AddTab('Main')
+	Main = window:AddTab("Main"),
 }
 
-local mainleftgroupbox = Tabs.Main:AddLeftGroupbox('Automatic stuff')
-mainleftgroupbox:AddDropdown('SelectOre',{
-    Values = {"Mythril","Gold","Iron","Pyrite","Tin","Copper"},
-    Default = 1,
-    Multi = false,
-    Text = "Ores",
-    Tooltip = "Click the type of ore to show edit its title or box visibility."
+local mainleftgroupbox = Tabs.Main:AddLeftGroupbox("Automatic stuff")
+mainleftgroupbox:AddDropdown("SelectOre", {
+	Values = { "Mythril", "Gold", "Iron", "Pyrite", "Tin", "Copper" },
+	Default = 1,
+	Multi = false,
+	Text = "Ores",
+	Tooltip = "Click the type of ore to show edit its title or box visibility.",
 })
-mainleftgroupbox:AddToggle('ShowBox',{
-    Text = "Show Box",
-    Default = false,
-    Tooltip = "Shows esp box"
+mainleftgroupbox:AddToggle("ShowBox", {
+	Text = "Show Box",
+	Default = false,
+	Tooltip = "Shows esp box",
 })
-mainleftgroupbox:AddToggle('Showtitle',{
-    Text = "Show Title",
-    Default = false,
-    Tooltip = "Shows esp title"
+mainleftgroupbox:AddToggle("Showtitle", {
+	Text = "Show Title",
+	Default = false,
+	Tooltip = "Shows esp title",
 })
 Toggles.ShowBox:OnChanged(function()
-    __Variables[selected] = Toggles.ShowBox.Value
+	__Variables[selected] = Toggles.ShowBox.Value
 end)
 
 Toggles.Showtitle:OnChanged(function()
-    __Variables[selected.."title"] = Toggles.Showtitle.Value
+	__Variables[selected .. "title"] = Toggles.Showtitle.Value
 end)
 
 Options.SelectOre:OnChanged(function()
-    selected = Options.SelectOre.Value
-    local title = __Variables[selected.."title"] -- quick fix
-    local box = __Variables[selected] -- quick fix
-    Toggles.Showtitle:SetValue(title)
-    Toggles.ShowBox:SetValue(box)
+	selected = Options.SelectOre.Value
+	local title = __Variables[selected .. "title"] -- quick fix
+	local box = __Variables[selected] -- quick fix
+	Toggles.Showtitle:SetValue(title)
+	Toggles.ShowBox:SetValue(box)
 end)
